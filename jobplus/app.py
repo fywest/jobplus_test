011587fc067d3e608b1c12fc67c34f61c3cbcade
fywest <fywest2109@hotmail.com>
2018-07-26 21:27:21 +0300

    update app.py for register more route in blueprints

--- jobplus/app.py
+++ jobplus/app.py
@@ -1,9 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask
 from flask_migrate import Migrate
-from flask_login import LoginManager
-
 from jobplus.config import configs
 from jobplus.models import db, User
+from flask_login import LoginManager
 
 
 def register_extensions(app):
@@ -19,36 +18,20 @@ def register_extensions(app):
     login_manager.login_view = 'front.login'
 
 
-def register_bluprints(app):
-    from .handlers import blueprints
-    for bp in blueprints:
-        app.register_blueprint(bp)
-
-
-def register_error_hanlers(app):
-    """ 因为使用接口通信，出错时也返回 JSON 数据
-    """
-
-    @app.errorhandler(404)
-    def not_found(error):
-        return render_template('error/404.html'), 404
-
-    @app.errorhandler(500)
-    def server_error(error):
-        return render_template('error/500.html'), 500
+def register_blueprints(app):
+    from .handlers import front, admin, user, company, job
+    app.register_blueprint(front)
+    app.register_blueprint(admin)
+    app.register_blueprint(user)
+    app.register_blueprint(company)
+    app.register_blueprint(job)
 
 
 def create_app(config):
-    """ App 工厂"""
     app = Flask(__name__)
-
-    if isinstance(config, dict):
-        app.config.update(config)
-    else:
-        app.config.from_object(configs.get(config, None))
+    app.config.from_object(configs.get(config))
 
     register_extensions(app)
-    register_bluprints(app)
-    register_error_hanlers(app)
+    register_blueprints(app)
 
     return app
