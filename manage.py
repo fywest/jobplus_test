abf9059483fece25e498be9fe04f594341f410f1
fywest <fywest2109@hotmail.com>
2018-08-04 15:43:46 +0300

    add coding utf8 at first line

--- manage.py
+++ manage.py
@@ -1,5 +1,13 @@
 from jobplus.app import create_app
 
+import sys
+ver=sys.version
+
+if int(ver[0])<3:
+    reload(sys)
+    sys.setdefaultencoding('utf-8')
+
+
 app = create_app('development')
 
 
