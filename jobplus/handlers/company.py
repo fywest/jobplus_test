dcacf70654da6cd87e3fcfe2d77479fa7e1bcbe3
fywest <fywest2109@hotmail.com>
2018-08-04 15:42:55 +0300

    add coding utf8 at first line

--- jobplus/handlers/company.py
+++ jobplus/handlers/company.py
@@ -1,3 +1,4 @@
+# -*- coding:utf8 -*-
 from flask import Blueprint, render_template, flash, redirect, url_for, request, abort, current_app
 from flask_login import login_required, current_user
 from jobplus.forms import CompanyProfileForm, JobForm
