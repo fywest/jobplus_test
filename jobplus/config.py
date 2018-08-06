51e01c219c2bffe58e3006c03fe9e408a7ea69d8
fywest <fywest2109@hotmail.com>
2018-08-06 18:34:42 +0300

    louplus mysql connection

--- jobplus/config.py
+++ jobplus/config.py
@@ -7,9 +7,7 @@ class BaseConfig(object):
 
 class DevelopmentConfig(BaseConfig):
     DEBUG = True
-    # SQLALCHEMY_DATABASE_URI = 'mysql+mysqldb://root@localhost:3306/jobplus?charset=utf8'#shiyanlou
-    SQLALCHEMY_DATABASE_URI = 'mysql+mysqldb://fywest:990113@localhost:3306/jobplus?charset=utf8'#pycharm
-    #SQLALCHEMY_DATABASE_URI = 'mysql+mysqldb://root:990113@localhost:3306/jobplus?charset=utf8'#ubuntu
+    SQLALCHEMY_DATABASE_URI = 'mysql+mysqldb://root@localhost:3306/jobplus?charset=utf8'#shiyanlou
 
 
 
